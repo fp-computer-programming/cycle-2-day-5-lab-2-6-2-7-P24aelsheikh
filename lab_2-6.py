4901bde3f36567cb3264d9008a1be4ab5842fd4e
P24aelsheikh <144148097+P24aelsheikh@users.noreply.github.com>
2023-09-21 12:19:30 -0400

    Add files via upload

---
+++ lab_2-6.py
@@ -0,0 +1,20 @@
+a=5
+if (a % 2) ==0:
+    print('a is even.')
+else:
+    print('a is odd')
+b=10
+if (b % 2) ==0:
+    print('b is even.')
+else:
+    print('b is odd')
+c=0
+if (c % 2) ==0:
+    print('c is even.')
+else:
+    print('c is odd')
+d=-4
+if (d % 2) ==0:
+    print('d is even.')
+else:
+    print('d is odd')
